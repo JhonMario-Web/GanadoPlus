ef363eca1c491a2bf9fa2a14a500da557e706a8f
Jhon Argumedo <jargumedo@activos.com.co>
2023-05-04 21:35:24 -0500

    Commit Inicial

---
+++ src/main/java/com/ganadoplus/GanadoPlusApplication.java
@@ -0,0 +1,13 @@
+package com.ganadoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanadoPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanadoPlusApplication.class, args);
+	}
+
+}
